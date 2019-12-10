79ec23e530fc7d43f6f1ca2a8c867f1a1b72bfac
Christian T Flink <chri62aa@edu.easj.dk>
2019-12-10 14:29:32 +0100

    seperated data into data.js

---
+++ data.js
@@ -0,0 +1,39 @@
+var insertRunes = "INSERT INTO runes (name, lvlreq, wpneffect, armeffect) VALUES ?";
+var runeValues = [
+  ['El', 11, '+50 To Attack Rating, +1 Light Radius', '+15 Defense, +1 To Light Radius'],
+  ['Eld', 11, '+75% Damage To Undead, +50 Attack Rating Against Undead', '15% Slower Stamina Drain/7% Increased Chance of Blocking(Shields)'],
+  ['Tir', 13, '+2 To Mana After Each Kill', '+2 To Mana After Each Kill'],
+  ['Nef', 13, 'Knockback', '+30 Defense Vs. Missile'],
+  ['Eth', 15, '-25% To Target Defense', 'Regenerate Mana 15%'],
+  ['Ith', 15, '+9 To Maximum Damage', '15% Damage Taken Goes to Mana'],
+  ['Tal', 17, '+75 Poison Damage Over 5 Seconds', 'Poison Resist 30%/Poison Resist 35%(Shields)'],
+  ['Ral', 19, 'Adds 5-30 Fire Damage', 'Fire Resist 30%/Fire Resist 35%(Shields)'],
+  ['Ort', 21, 'Adds 1-50 Lightning Damage', 'Lightning Resist 30%/Lightning Resist 35%(Shields)'],
+  ['Thul', 23, 'Adds 3-14 Cold Damage - 3 Second Duration', 'Cold Resist 30%/Cold Resist 35%(Shields)'],
+  ['Amn', 25, '7% Life Stolen Per Hit', 'Attacker Takes Damage of 14'],
+  ['Sol', 27, '+9 To Minimum Damage', 'Damage Reduced By 7'],
+  ['Shael', 29, '20% Increased Attack Speed', '20% Faster Hit Recovery/20% Faster Block Rate(Shields)'],
+  ['Dol', 31, 'Hit Causes Monster To Flee 25%', 'Replenish Life +7'],
+  ['Hel', 0, 'Requirements -20%', 'Requirements -15%'],
+  ['Io', 35, '+10 To Vitality', '+10 To Vitality'],
+  ['Lum', 37, '+10 To Energy', '+10 To Energy'],
+  ['Ko', 39, '+10 To Dexterity', '+10 To Dexterity'],
+  ['Fal', 41, '+10 To Strength', '+10 To Strength'],
+  ['Lem', 43, '75% Extra Gold From Monsters', '50% Extra Gold From Monsters'],
+  ['Pul', 45, '+75% Damage To Demons, +100 Attack Rating Against Demons', '+30% Enhanced Defense'],
+  ['Um', 47, '25% Chance of Open Wounds', 'All Resistances +15(Armor/Helms) +22(Shields)'],
+  ['Mal', 49, 'Prevent Monster Heal', 'Magic Damage Reduced By 7'],
+  ['Ist', 51, '30% Better Chance of Getting Magic Items', '25% Better Chance of Getting Magic Items'],
+  ['Gul', 53, '20% Bonus To Attack Rating', '5% To Maximum Poison Resist'],
+  ['Vex', 55, '7% Mana Stolen Per Hit', '5% To Maximum Fire Resist'],
+  ['Ohm', 57, '+50% Enhanced Damage', '5% To Maximum Cold Resist'],
+  ['Lo', 59, '20% Deadly Strike', '5% To Maximum Lightning Resist'],
+  ['Sur', 61, 'Hit Blinds Target', 'Maximum Mana 5%/+50 To Mana (Shields)'],
+  ['Ber', 63, '20% Chance of Crushing Blow', 'Damage Reduced by 8%'],
+  ['Jah', 65, 'Ignore Targets Defense', 'Increase Maximum Life 5%/+50 Life (Shields)'],
+  ['Cham', 67, 'Freeze Target +3', 'Cannot Be Frozen'],
+  ['Zod', 69, 'Indestructible', 'Indestructible']
+];
+
+export { runeValues };
+export {insertRunes};
