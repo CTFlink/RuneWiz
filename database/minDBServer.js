66b636c4276f6af68ae954ef3b504a45bdf4666c
Christian T Flink <chri62aa@edu.easj.dk>
2019-12-15 13:33:47 +0100

    clean up code indentations

--- database/minDBServer.js
+++ database/minDBServer.js
@@ -1,6 +1,10 @@
 const data = require('./data.js');
-
 var mysql = require('mysql');
+var createDB = "CREATE DATABASE IF NOT EXISTS runewords"
+var createTable = "CREATE TABLE IF NOT EXISTS runes (name VARCHAR(255), lvlreq INT, wpneffect VARCHAR(255), armeffect VARCHAR(255), selected INT)";
+var insertRunes = "INSERT INTO runes (name, lvlreq, wpneffect, armeffect, selected) VALUES ?";
+var selectRunes = "SELECT * from runes";
+
 
 //Connect to database
 var con = mysql.createConnection({
@@ -15,24 +19,56 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
-con.query("CREATE DATABASE IF NOT EXISTS runewords", function (err, result) {
+con.query(createDB, function (err, result) {
   if (err) throw err;
   console.log("Database created");
 });
 
-var sql = "CREATE TABLE IF NOT EXISTS runes (name VARCHAR(255), lvlreq INT, wpneffect VARCHAR(255), armeffect VARCHAR(255), selected INT)";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Table created");
-  });
+con.query(createTable, function (err, result) {
+  if (err) throw err;
+  console.log("Table created");
+});
 
-  //Inserting rune values into database
-  var insertRunes = "INSERT INTO runes (name, lvlreq, wpneffect, armeffect, selected) VALUES ?";
+con.query(insertRunes, [data.runeValues], function (err, result) {
+  if (err) throw err;
+  console.log("Number of records inserted: " + result.affectedRows);
+});
 
-  con.query(insertRunes, [data.runeValues], function (err, result) {
+con.query(selectRunes, function (err, result) {
+  if (err) throw err;
+
+  // Loop on each row
+  var lng = result.length;
+  var rc = result;
+  for (var i = 0; i < lng; i++) {
+
+    //Create an object to save current row's data
+    var runeobj = {
+      'name':rc[i].name,
+      'lvlReq':rc[i].lvlreq,
+      'wpnEffect':rc[i].wpneffect,
+      'armEffect':rc[i].armeffect,
+      'selected':rc[i].selected,
+    }
+    console.log(runeobj);
+  }
+});
+
+//Drop db
+function dropDB(){ 
+  con.query('DROP DATABASE [IF EXISTS] runes', function (err, result) {
     if (err) throw err;
-    console.log("Number of records inserted: " + result.affectedRows);
-  });
+    });
+    console.log("Databasen er slettet!")
+}
+
+//Create an object to save current row's data
+
+            // 'name':[i].
+            // 'lvlReq =
+            // 'wpnEffect =
+            // 'armEffect =
+            // 'chosen =
 
   function visHej(){
     alertNumber("hej")
